31abe589a1c3e5ec615369947101c3998682ba76
sandeepkumar-84 <36576549+sandeepkumar-84@users.noreply.github.com>
2025-01-06 22:44:29 +0000

    till arrays

--- basics_01.js
+++ basics_01.js
@@ -31,7 +31,7 @@ you don't need to declare the data type of a variable explicitly
 
 // slice 
 
-console.log(name.slice(0,2));
+// console.log(name.slice(0,2));
 
 // Math library 
 
@@ -51,15 +51,96 @@ console.log(`random value between 10 and 20 = ${rn2}`);
 
 // datetime
 
-let dt  = new Date()
+/*let dt  = new Date()
 
 console.log(dt);
 console.log(dt.toString());
 console.log(dt.toDateString());
 console.log(dt.toLocaleDateString());
+*/
+
+// arrays
+
+let myarray = [0,1,2,3,4,5]
+
+/*
+myarray.push(6)
+myarray.pop(6)
+
+console.log(myarray);
+
+console.log(myarray.unshift(7));
+
+console.log(myarray);
+
+console.log(myarray.shift());
+
+console.log(myarray);
+
+console.log(myarray.includes(4));
 
 
+let jonedArr = myarray.join()
+
+console.log(jonedArr);
+*/
+
+// diff between slice and splice - splice changes the actual array also. 
+
+/*
+console.log("before");
+
+console.log(myarray);
+
+const myarray2 = myarray.slice(1,3)
+
+console.log(myarray2);
+
+const myarray3 = myarray.splice(1,3)
+
+console.log(myarray2);
+
+console.log("after splice ");
+
+console.log(myarray);
+
+*/
+
+// Arrays merge etc
+
+/*
+let arr1 = [0,1,2,3]
+let arr2 = [4,5,6,7]
 
 
+const arr4 = ['a','b','c']
+const arr5 = ['e','f','g']
 
 
+arr1.push(arr2)
+arr4.push(arr5)
+console.log(arr1)
+console.log(arr4)
+
+
+arr6 = arr4.concat(arr5)
+
+console.log(arr6);
+
+// Spread out & flat
+const arrMerge = [...arr4, ...arr5]
+
+console.log(arrMerge);
+
+// flat 
+
+arrComplex = [  1,2,3,[4,5,6],7,[8,9,[1,2]]]
+
+arrFlat = arrComplex.flat(Infinity)
+
+console.log(arrFlat);
+
+console.log(Array.from("arrayexample"))
+console.log(Array.of(name,age,sex))
+
+*/
